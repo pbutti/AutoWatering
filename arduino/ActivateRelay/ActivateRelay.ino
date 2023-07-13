8014441754aa21e5f6a08af7797c550c561c7006
pbutti <pierfrancesco.butti@protonmail.com>
2023-07-13 14:30:27 +0200

    Both pins

--- arduino/ActivateRelay/ActivateRelay.ino
+++ arduino/ActivateRelay/ActivateRelay.ino
@@ -8,9 +8,14 @@ void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
 }
 
 void loop() {
+
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) { 
    incomingByte = Serial.read();
@@ -26,11 +31,13 @@ void loop() {
 
   if (turnOff && isOn) {
     digitalWrite(6,LOW);
+    digitalWrite(7,LOW);
     isOn = false;
   }
 
   if (!turnOff && !isOn) {
-  digitalWrite(6,HIGH); 
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
   isOn = true;
   }
 }
